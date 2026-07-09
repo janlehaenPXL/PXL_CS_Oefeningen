a615268e2ebebb546376c517a440145510ec6233
Jan Lehaen <jan.lehaen@student.pxl.be>
2026-07-09 19:56:24 +0200

    Added input validation

--- ESS/Variables/Exercises/FahrenheitToCelsius/FahrenheitToCelsius/Program.cs
+++ ESS/Variables/Exercises/FahrenheitToCelsius/FahrenheitToCelsius/Program.cs
@@ -8,10 +8,22 @@
             double celsius = 0;
             Console.WriteLine("=+=+=+=+= Fahrenheit to Celsius Converter =+=+=+=+=");
             Console.Write("Degrees in Fahrenheit: ");
-            double.TryParse(Console.ReadLine(), out fahrenheit);
-            celsius = (fahrenheit - 32) * 5 / 9;
 
-            Console.WriteLine($"{fahrenheit} F° is {celsius} C°.");
+            //input validation
+            if (!double.TryParse(Console.ReadLine(), out fahrenheit))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input. Restart to try again");
+                Console.ResetColor();
+            }
+            else
+            {
+                //callculation
+                celsius = (fahrenheit - 32) * 5 / 9;
+
+                Console.WriteLine($"{fahrenheit} F° is {celsius} C°.");
+            }
+                
         }
     }
 }
