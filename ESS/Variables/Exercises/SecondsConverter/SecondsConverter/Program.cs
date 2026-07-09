49d0ebeb4ae95ad062581702aff60d311a9a3bcf
Jan Lehaen <jan.lehaen@student.pxl.be>
2026-07-09 20:48:49 +0200

    Seconds convertor added

---
+++ ESS/Variables/Exercises/SecondsConverter/SecondsConverter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SecondsConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int seconds;
+
+            Console.WriteLine("=+=+=+= Seconds Converter =+=+=+= ");
+            Console.Write("\nEnter the number of seconds to convert: ");
+            if(!int.TryParse(Console.ReadLine(), out seconds))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input. Please enter a valid number of seconds.");
+                Console.ResetColor();
+            }
+            else
+            {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int remainingSeconds = seconds % 60;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\n{seconds} seconds is equal to {hours} hours, {minutes} minutes, and {remainingSeconds} seconds.");
+                Console.ResetColor();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
