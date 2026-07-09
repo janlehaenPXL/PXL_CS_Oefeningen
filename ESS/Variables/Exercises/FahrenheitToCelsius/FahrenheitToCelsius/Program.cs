00c14de4f5bf0d5f45589c7a59cad5f83efe8421
Jan Lehaen <jan.lehaen@student.pxl.be>
2026-07-09 18:24:28 +0200

    Fahrenheit converter

---
+++ ESS/Variables/Exercises/FahrenheitToCelsius/FahrenheitToCelsius/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FahrenheitToCelsius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit = 0;
+            double celsius = 0;
+            Console.WriteLine("=+=+=+=+= Fahrenheit to Celsius Converter =+=+=+=+=");
+            Console.Write("Degrees in Fahrenheit: ");
+            double.TryParse(Console.ReadLine(), out fahrenheit);
+            celsius = (fahrenheit - 32) * 5 / 9;
+
+            Console.WriteLine($"{fahrenheit} F° is {celsius} C°.");
+        }
+    }
+}
---
+++ ESS/Variables/Exercises/Rekenmachine/Rekenmachine/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Rekenmachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            Console.WriteLine("=+=+=+=+= CALCULATOR =+=+=+=+=");
+            Console.Write("Your first number: ");
+            int.TryParse(Console.ReadLine(), out firstNumber);
+            Console.Write("Your second number: ");
+            int.TryParse(Console.ReadLine(), out secondNumber);
+
+            Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is {firstNumber + secondNumber}");
+
+        }
+    }
+}
