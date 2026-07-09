b689e0bea275097c9c2c13dc8f67c1ef838fdd68
Jan Lehaen <jan.lehaen@student.pxl.be>
2026-07-09 20:05:27 +0200

    Checkout exercises added

---
+++ ESS/Variables/Exercises/Checkout/Checkout/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Checkout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // Set output encoding to UTF-8 for Euro symbol support
+
+            decimal price = 0.0m;
+            decimal amount = 0.0m;
+            Console.WriteLine("=+=+=+=+= CHECKOUT =+=+=+=+=");
+            Console.Write("Enter the price of the item (00,00):");
+            if (!decimal.TryParse(Console.ReadLine(), out price))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input. Restart to try again.");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Write("Item amount: ");
+                if (!decimal.TryParse(Console.ReadLine(), out amount))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input. Restart to try again.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    decimal total = price * amount;
+                    Console.WriteLine($"Total: €{total}");
+                }
+            }
+        }
+    }
+}
