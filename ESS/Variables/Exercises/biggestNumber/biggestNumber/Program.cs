d0d30230e45f0c83d32b00f82664beaa6539a576
Jan Lehaen <jan.lehaen@student.pxl.be>
2026-07-10 15:58:38 +0200

    Biggest number done and added

---
+++ ESS/Variables/Exercises/biggestNumber/biggestNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace biggestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 0;
+            int number2 = 0;
+
+            Console.WriteLine("=+=+=+=+= Biggest number =+=+=+=+=\n");
+            Console.WriteLine("Enter 2 integer numbers and I wil give you the biggist one back.");
+            Console.Write("First number: ");
+            if(!int.TryParse(Console.ReadLine(), out number1))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input, restart to try again.");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.Write("Second number: ");
+                if(!int.TryParse(Console.ReadLine(), out number2))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input, restart to try again.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    if(number1 > number2)
+                    {
+                        Console.WriteLine($"{number1} is the biggest number.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number2} is the biggest number.");
+                    }
+                }
+            }
+
+        }
+    }
+}
