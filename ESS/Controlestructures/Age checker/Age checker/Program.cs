b7d26897b464aba7ed124d327888e4e3e5b8a7be
Jan Lehaen <jan.lehaen@student.pxl.be>
2026-07-13 11:03:35 +0200

    Age checker done!

---
+++ ESS/Controlestructures/Age checker/Age checker/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Age_checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=+=+=+=+= Checking age =+=+=+=+=\n");
+            int birthYear = 0;
+            int currentYear = DateTime.Now.Year;
+            bool adult = false;
+
+
+            Console.Write("Enter your birthyear: ");
+            if (!int.TryParse(Console.ReadLine(), out birthYear) || birthYear < 1900 || birthYear > currentYear)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nInvalid input, restart to try again.");
+                Console.ResetColor();
+                return;
+            }
+
+            int age = currentYear - birthYear;
+                     
+            Console.WriteLine($"\nYou were born in {birthYear} and it now is {currentYear}, so you are {age} years old!");
+            if (age > 18)
+            {
+                Console.WriteLine("You are over 18, so you're an adult!");
+            }
+            else
+            {
+                Console.WriteLine("You are not yet an adult!");
+            }
+
+
+
+        }
+    }
+}
