b24858ccf91d3cf1988ee590dde22fa190695fc0
janlehaenPXL <jan.lehaen@student.pxl.be>
2026-07-21 23:43:37 +0200

    DiscountrCounter Done

---
+++ ClaudeExercise/ESS/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            double price;
+            double discount;
+            double newPrice;
+            double total;
+
+            Console.WriteLine("=+=+=+=+= Discount =+=+=+=+=\n");
+            Console.WriteLine("Enter the price and amount of discount!\n");
+            Console.Write("Price: ");
+            if (!double.TryParse(Console.ReadLine(), out price))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input, restart to try again");
+                Console.ResetColor();
+                return;
+
+            }
+
+            Console.Write("Discount in %: ");
+            if (!double.TryParse(Console.ReadLine(), out discount))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input, restart to try again");
+                Console.ResetColor();
+                return;
+            }
+
+            newPrice = Math.Round(price - (price * (discount / 100)), 2);
+
+            Console.WriteLine($"€{price:F2} - {discount}% = €{newPrice:F2}");
+
+
+
+        }
+
+
+    }
+}
---
+++ ESS/Controlestructures/enterpriseNumber/enterpriseNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace enterpriseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string enterpriceNumber;
+            bool isValid = false;
+
+            Console.WriteLine("=+=+=+=+= Enterprise number =+=+=+=+=\n");
+            do
+            {
+                Console.Write("Enter your enterprice number: ");
+                enterpriceNumber = Console.ReadLine();
+                enterpriceNumber.Replace(".", "");
+
+               
+                if (!int.TryParse(enterpriceNumber, out int enterpriceNr))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input, only numbers allowed!");
+                    Console.ResetColor();
+                }
+                else if (enterpriceNumber.Length != 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input, must me exact 10 number");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    isValid = true;
+                }
+            } while (!isValid);
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}
