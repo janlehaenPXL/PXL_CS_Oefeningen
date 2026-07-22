f18bf3f7389cc80dcc6edf13f80f2270f0600d51
janlehaenPXL <jan.lehaen@student.pxl.be>
2026-07-22 13:34:08 +0200

    Countto20 done

---
+++ ClaudeExercise/ESS/CountTo20Without3s/CountTo20Without3s/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace CountTo20Without3s
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a console program that prints the numbers 1 to 20, but:
+            //skips all multiples of 3(do not print, but continue)
+            //stops completely as soon as you reach 15
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                if ((i % 3) == 0)
+                {
+                    if (i == 15)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                       
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
