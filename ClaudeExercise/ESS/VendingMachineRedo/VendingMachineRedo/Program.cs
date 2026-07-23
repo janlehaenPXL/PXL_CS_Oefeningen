062fc02ebdf09e8a5faf91eecaaf4c1b771d6e5f
janlehaenPXL <jan.lehaen@student.pxl.be>
2026-07-23 13:18:18 +0200

    KnighVSGoblin V1.0 done

---
+++ ClaudeExercise/ESS/VendingMachineRedo/VendingMachineRedo/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace VendingMachineRedo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userChoise;
+            int userInsert;
+            double toPay = 0.00;
+            double insertAmount = 0.00;
+            double change;
+
+
+            //drinks price
+            double sodaPrice = 2.20;
+            double waterPrice = 2.00;
+
+            //coins value
+            double coin200 = 2.00;
+            double coin100 = 1.00;
+            double coin50 = 0.50;
+            double coin20 = 0.20;
+            double coin10 = 0.10;
+            double coin5 = 0.05;
+            double coin2 = 0.02;
+            double coin1 = 0.01;
+
+
+            //coins amount
+            int coin200Amnt;
+            int coin100Amnt;
+            int coin50Amnt;
+            int coin20Amnt;
+            int coin10Amnt;
+            int coin5Amnt;
+            int coin2Amnt;
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("=+=+=+=+= VENDING MACHINE 2.0 =+=+=+=+=\n");
+            Console.WriteLine($"1: Cola 330ml:\t\t€{sodaPrice:F2}\n2: Cola zero 330ml:\t€{sodaPrice:F2}\n3: Fanta 330ml:\t\t€{sodaPrice:F2}\n4: Sprite 330ml:\t€{sodaPrice:F2}\n5: Water plat 500ml:\t€{waterPrice:F2}\n6: Water bruis 500ml:\t€{waterPrice:F2}\n\n");
+
+            do
+            {
+                Console.WriteLine("Your choise: ");
+
+            } while (!int.TryParse(Console.ReadLine(), out userChoise) || userChoise < 1 && userChoise > 6);
+
+            switch (userChoise)
+            {
+                case 1:
+                    toPay = sodaPrice;
+                    break;
+                case 2:
+                    toPay = sodaPrice;
+                    break;
+                case 3:
+                    toPay = sodaPrice;
+                    break;
+                case 4:
+                    toPay = sodaPrice;
+                    break;
+                case 5:
+                    toPay = waterPrice;
+                    break;
+                case 6:
+                    toPay = waterPrice;
+                    break;
+            }
+
+            Console.WriteLine($"That will be €{toPay:F2}.");
+
+            //Inserting coins
+            Console.WriteLine("Coins:\n1:€2\n2:€1\n3:€0.50\n4:€0.20\n5:€0.10\n6:€0.05\n7:€0.02\n8:€0.01 ");
+            do
+            {
+                Console.WriteLine($"Insert amount: {insertAmount:F2}\nInsert: ");
+                if(int.TryParse(Console.ReadLine(), out userInsert)  && userInsert > 0 && userInsert <= 8)
+                {
+                    switch (userInsert)
+                    {
+                        case 1:
+                            insertAmount = insertAmount + coin200;
+                            break;
+                        case 2:
+                            insertAmount += coin100;
+                            break;
+                        case 3:
+                            insertAmount += coin50;
+                            break;
+                        case 4:
+                            insertAmount += coin10;
+                            break;
+                        case 5:
+                            insertAmount += coin20;
+                            break;
+                        case 6:
+                            insertAmount += coin5;
+                            break;
+                        case 7:
+                            insertAmount += coin2;
+                            break;
+                        case 8:
+                            insertAmount += coin1;
+                            break;
+                    }
+                }
+                
+
+            } while (insertAmount < toPay);
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ESS/Loops/Labo/KnightVsGoblin/KnightVsGoblin/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace KnightVsGoblin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+                                                                                                   
+            )(          KNIGHT                       GOBLIN          ||
+   O-)///////(**)====================>   VS   <====================(**)\\\\\\\)=O
+            )(                                                       ||
+    
+    ==========================================================================
+");
+            Random rng = new Random();
+            //Bool for knightshealth loop
+            bool isValidHealth = false;
+            //Bool for choise menu loop
+            bool isValidMove = false;
+            int playerChoise = 0;
+
+            //stats
+            int goblinHealth = rng.Next(0,101);
+            int knightHealth;
+
+            int goblinATK;
+            int knightATK = 10;
+            int healingPotion = 10;
+
+            Console.WriteLine("What is the health of you knight?");
+            do
+            {
+                Console.Write("Health: ");
+                if(!int.TryParse(Console.ReadLine(), out knightHealth) || knightHealth < 0 || knightHealth > 100)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Pleas enter a valid number between 0 and 100!");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    isValidHealth = true;
+                }
+            }while(!isValidHealth);
+
+           while (knightHealth > 0 && goblinHealth > 0)
+            {
+                do
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"\nKnight health:\t{knightHealth}\nGoblin health: \t{goblinHealth}");
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\nDo you want to do:\n ");
+                    Console.ResetColor();
+                    Console.WriteLine("1: attack\n2: heal");
+
+                    if(!int.TryParse(Console.ReadLine(), out playerChoise) || playerChoise != 1 && playerChoise != 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("invalid choise, only 1 or 2.");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        isValidMove = true;
+                    }
+                    goblinATK = rng.Next(5, 17);
+
+
+                    switch (playerChoise)
+                    {
+                        case 1:
+                            knightHealth -= goblinATK;
+                            goblinHealth -= knightATK;
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"You attacked the goblim for {knightATK} damage!\nThe goblin attacked you with {goblinATK} damage! ");
+                            Console.ResetColor();
+                            break;
+                        case 2:
+                            knightHealth += healingPotion;
+                            knightHealth -= goblinATK;
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"You took a healing potion and gained {healingPotion} health points back!\nThe goblin attacked you with {goblinATK} damage! ");
+                            Console.ResetColor();
+                            break;
+                    }
+
+
+                } while (!isValidMove);
+                
+
+            }
+            if (knightHealth < 0)
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("\n\nYour warrior was slain in the battle with the goblin...");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine("\n\nThe faul goblin was slain in battle! You won!!");
+                Console.ResetColor();
+            }
+
+            
+
+
+
+
+
+        }
+    }
+}
